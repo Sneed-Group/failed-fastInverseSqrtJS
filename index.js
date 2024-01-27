24fb7c44eafadf13b05f2cc13ea171adc128e625
Samuel Lord <9904667+NodeMixaholic@users.noreply.github.com>
2024-01-27 13:49:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-//wow.
-function fastInvSqrt(x) {
-    var xhalf = 0.5 * x;
-    var i = x;
-    i = i * (1.5 - xhalf * i * i);
-    i = i * (1.5 - xhalf * i * i);
-    return i;
+//now even more amazing!
+function fastInverseSqrt(number) {
+    const threehalfs = 1.5;
+    const x2 = number * 0.5;
+    let i = new Float32Array(1);
+    i[0] = number;
+    let y = new Int32Array(i.buffer);
+    y = threehalfs - (x2 * y * y);
+    return y;
 }
